4664b26bd886dadb8b0cbcbac1cec8eafdca67f8
Spureddy <reddyspurthi53@gmail.com>
2025-11-18 04:44:48 +0530

    simple intrest.c

---
+++ simple intrest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  float p,t,r,si;
+printf("enter the value of principle\n");
+scanf("%f",&p);
+printf("enter the value of time\n");
+scanf("%f",&t);
+printf("enter the value of rate if intrest\n");
+scanf("%f",&r);
+si=(p*t*r)/100
+  printf("simple intrest=%.2f",simple intrest);
+return 0
+  }
