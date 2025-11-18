c27dd313ff4e9308c8dbe3a35414e6053e849a88
Spureddy <reddyspurthi53@gmail.com>
2025-11-18 06:17:51 +0530

    area of rectangle.c

---
+++ area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  float=l,w,area;
+  printf("enter the value of l\n");
+  scanf("%f",&l);
+  printf("enter the value of w\n");
+  scanf("%f",&w);
+  area=l*w;
+  printf("area=%.2f\n",area);
+return 0
+  }
